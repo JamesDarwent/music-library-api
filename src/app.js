44e30dce011a8e44068ba99783b26982d5819ace
James Darwent <jamesdarwent_88@hotmail.co.uk>
2021-03-17 18:17:02 +0000

    Reading single artists completed

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
-
-
 const artistControllers = require('./controllers/artists');
 
 
-app.post('/artists', artistControllers.create);
+const app = express();
 
+app.use(express.json());
+
+
+
+app.post('/artists', artistControllers.create);
 app.get('/artists', artistControllers.list);
+app.get('/artists/:artistId', artistControllers.getArtistById);
+
 
 
 
